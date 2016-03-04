19afd0955593b1c5782d98ff62f458ade31a2eab
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2016-03-04 22:00:04 +0100

    Añadiendo menu

---
+++ RPG_MoonOfStone/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace RPG_MoonOfStone
+{
+    class RPG_MoS
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            int opcion = 0;
+            string nombre = string.Empty;
+
+            while (opcion != 4)
+            {
+                Console.WriteLine("Menu para RPG");
+                Console.WriteLine();
+                Console.WriteLine("1. Elegir nombre");
+                Console.WriteLine("2. Elegir profesion");
+                Console.WriteLine("3. Elegir nombre del mundo");
+                Console.WriteLine("4. Salir");
+                Console.WriteLine();
+
+                Console.Write("Elige tu opcion");
+                opcion = ObtenerNum();
+            }
+        }
+
+        static int ObtenerNum()
+        {
+            int num = 0;
+            bool valido = false;
+
+            while (!valido)
+            {
+                try
+                {
+                    num = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Numero no valido");
+                    Console.WriteLine("Intentalo de nuevo");
+                }
+            }
+            return num;
+        }
+    }
+}
