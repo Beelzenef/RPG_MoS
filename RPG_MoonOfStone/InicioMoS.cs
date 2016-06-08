332d97c42e500083e1ff467d2da05ab21a898f40
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2016-06-08 16:56:24 +0200

    Update en Menu, por @Lfrty

--- RPG_MoonOfStone/InicioMoS.cs
+++ RPG_MoonOfStone/InicioMoS.cs
@@ -7,48 +7,145 @@ namespace RPG_MoonOfStone
         static void Main(string[] args)
         {
             Console.Title = "Moon of Stone";
-            MostrarMenu();
-            GestionOpciones();
+            ControlTeclas();
         }
 
-        
 
-        static void GestionOpciones()
+        #region Posicionamiento de cursor para recibir informacion
+        const int POS1 = 20;
+        const int POS2 = 15;
+
+        static void PosicionarCursor()
+        {
+            Console.SetCursorPosition(POS1, POS2);
+        }
+        #endregion
+
+        static void ControlTeclas()
         {
             ConsoleKeyInfo opcion;
-            string nombre = string.Empty;
-            PJ personaje = new PJ();
-            do {
+
+            do
+            {
+                UIMenu();
+                PosicionarCursor();
                 opcion = Console.ReadKey();
                 switch (opcion.KeyChar)
                 {
                     case '1':
-                        personaje.ModificarNombre();
-                        Console.WriteLine(personaje.Nombre);
+                        PosicionarCursor();
+                        Console.WriteLine("Has seleccionado Circulear");
                         break;
                     case '2':
-                        Console.WriteLine("Eleccion de profesion");
+                        PosicionarCursor();
+                        Console.WriteLine("Has seleccionado Atacar");
                         break;
                     case '3':
-                        Console.WriteLine("Eleccion de mundo");
+                        PosicionarCursor();
+                        Console.WriteLine("Has seleccionado Defender");
+                        break;
+                    case 'Q':
+                    case 'q':
+                        PosicionarCursor();
+                        Console.WriteLine("Fin del programa ...");
+                        return;
+                    default:
+                        Console.WriteLine("Por favor, elija entre las opciones mostradas");
                         break;
                 }
-                MostrarMenu();
+                Console.ReadKey();
+                Console.Clear();
             } while (opcion.Key != ConsoleKey.Q);
         }
 
-        static void MostrarMenu()
+        static void UIMenu()
         {
-            Console.WriteLine("Moon of Stone");
+
+            #region PintaMenu
+
+            int columna = 1;
+            const int limLaterales = 11;
+            const int limHorizontales = 60;
+
+            Console.Write("\t╔");
+            for (int i = 0; i < limHorizontales; i++)
+            {
+
+                Console.Write("═");
+
+            }
+            Console.Write("╗");
+
+            Console.SetCursorPosition(30, columna);
+            Console.WriteLine("Rythmatismo");
+            Console.SetCursorPosition(24, ++columna);
+            Console.WriteLine("1. Modificar circulo");
+            Console.SetCursorPosition(24, ++columna);
+            Console.WriteLine("2. Dibujar ataque");
+            Console.SetCursorPosition(24, ++columna);
+            Console.WriteLine("3. Dibujar defensa");
+            Console.SetCursorPosition(24, 7);
+            Console.WriteLine("Q. SALIR");
             Console.WriteLine();
-            Console.WriteLine("1. Elegir nombre");
-            Console.WriteLine("2. Elegir profesion");
-            Console.WriteLine("3. Elegir nombre del mundo");
-            Console.WriteLine("Q. Salir");
+            Console.SetCursorPosition(24, 9);
+            Console.Write("Por favor, pulsa una opción:");
+            Console.WriteLine();
             Console.WriteLine();
 
-            Console.WriteLine("Elige tu opcion");
-            Console.WriteLine();
+            Console.Write("\t╚");
+            for (int i = 0; i < limHorizontales; i++)
+            {
+
+                Console.Write("═");
+            }
+
+            Console.Write("╝");
+
+
+            for (int i = 1; i < limLaterales; i++)
+            {
+
+                Console.SetCursorPosition(69, i);
+                Console.WriteLine("║");
+            }
+
+            for (int i = 1; i < limLaterales; i++)
+            {
+                Console.SetCursorPosition(8, i);
+                Console.WriteLine("║");
+            }
+
+            #endregion
+
+            #region El menu de usuario
+
+            // Constantes para dibujar menu pequeño
+            const int largoIU = 37;
+            const int limIzqIU = 19;
+            const int limDerIU = 57;
+
+            Console.SetCursorPosition(limIzqIU, 14);
+            Console.Write("╔");
+            for (int i = 0; i < largoIU; i++)
+            {
+                Console.Write("═");
+            }
+            Console.Write("╗");
+
+            Console.SetCursorPosition(limIzqIU, 15);
+            Console.Write("║");
+            Console.SetCursorPosition(limIzqIU, 16);
+            Console.Write("╚");
+            for (int i = 0; i < largoIU; i++)
+            {
+                Console.Write("═");
+            }
+            Console.SetCursorPosition(limDerIU, 15);
+            Console.Write("║");
+            Console.SetCursorPosition(limDerIU, 16);
+            Console.Write("╝");
+
+            #endregion
         }
     }
 }
