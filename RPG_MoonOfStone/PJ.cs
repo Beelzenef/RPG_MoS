a825d350f73837385b1a5556af5465841c1ca652
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2016-06-08 17:01:40 +0200

    Personaje, Rythmatismo y Circulos iniciados

--- RPG_MoonOfStone/PJ.cs
+++ RPG_MoonOfStone/PJ.cs
@@ -9,13 +9,68 @@ namespace RPG_MoonOfStone
     class PJ
     {
         string _nombre;
+        int _nivel;
 
-        public string Nombre { get; set; }
+        public string Nombre { get { return _nombre; } set { _nombre = value; } }
 
         public void ModificarNombre()
         {
             Console.WriteLine("Escribe el nombre del personaje: ");
             Nombre = Console.ReadLine();
         }
+        public int Nivel
+        {
+            get { return _nivel; }
+            private set { _nivel = value; }
+        }
+
+    }
+
+    class Rythmatista : PJ
+    {
+        // Campos
+
+        TiposTiza _tiza;
+        CirculoInvocacion _circuloRythmatico;
+
+        // Propiedades
+
+        public TiposTiza Tiza
+        {
+            get { return _tiza; }
+            private set { _tiza = value; }
+        }
+
+        public CirculoInvocacion Circulo
+        {
+            get { return _circuloRythmatico; }
+            set { _circuloRythmatico = value; }
+        }
+
+        public bool Activacion { get; set; }
+
+        // Metodos
+
+        void DibujarCirculo()
+        {
+            Circulo = new CirculoInvocacion();
+            Activacion = true;
+        }
+
+        void DestruccionDeCirculo()
+        {
+            Circulo = null;
+        }
+
+        // Datos privados
+
+        public enum TiposTiza
+        {
+            Roja, Blanca, Azul
+        }
+    }
+
+    class CirculoInvocacion
+    {
     }
 }
